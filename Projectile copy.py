f6b1b45a04f7cae4b434fe38ba2f3797fc376573
jbarlian <jbarlian@aol.com>
2017-10-23 22:36:03 +0700

    HW Assignment

---
+++ Projectile copy.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+graphs = int(input("Enter the number of graphs: "))
+
+for i in range(0, graphs):
+    gravity = 9.81
+
+    velocity = int(input("Enter the initial Velocity (m/s): "))
+    angle = int(input("Enter the angle measured in degrees: "))
+    radian = (math.pi/180)*angle
+    time_flight = (2*velocity*(math.sin(radian)))/gravity
+    height = ((velocity**2)*(math.sin(radian)**2))/(2*gravity)
+    horizontal_range = ((velocity**2)*(math.sin(radian * 2)))/gravity
+
+    numx = []
+    numy = []
+
+    for i in (np.arange(0, int(time_flight), 0.01)):
+        x_distance = velocity * i
+        x_velocity = math.cos(radian) * velocity
+        y_distance = ((math.sin(radian) * velocity) * i) - (gravity * (i**2))/2
+        y_velocity = (math.sin(radian) * velocity) - (gravity * i)
+        numx.append(x_distance)
+        numy.append(y_distance)
+
+    plt.plot(numx, numy)
+
+plt.show()
